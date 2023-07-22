94c53c41cdebfd620f4eed7b5abb31eb582a52e0
dimavolo1 <125883080+dimavolo1@users.noreply.github.com>
2023-07-22 13:25:14 +0500

    commit

---
+++ src/test/java/org/example/StringUtilsTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+public class StringUtilsTest {
+
+    @Test
+    public void testIsEmpty() {
+        assertFalse(StringUtils.isEmpty("TEST"));
+        assertTrue(StringUtils.isEmpty(""));
+        assertTrue(StringUtils.isEmpty(" "));
+    }
+
+    @Test
+    public void testToDouble() {
+        assertEquals(3.1415, StringUtils.toDouble("3.1415"), 0.0001);
+        assertEquals(Double.NaN, StringUtils.toDouble(null), 0.00001);
+    }
+
+    @Test
+    public void testFromDouble() {
+        double source = 3.1415;
+        String expected="3.1415";
+        String actual = StringUtils.fromDouble(source);
+        assertEquals(expected, actual);
+    }
+}
